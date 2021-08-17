ffed4f588d0d8e73243a2357623054a58e5f7021
sgt-dope <sgtdopeaf@gmail.com>
2021-08-17 17:47:06 +0000

    added authentication

--- send.py
+++ send.py
@@ -1,15 +1,17 @@
 import os
 import time
 import requests
+from requests.auth import HTTPBasicAuth
 
 comnd = os.popen('gp url')
 curr_url = comnd.read().strip()
 
 data = {"url":curr_url}
 
-for i in range(2):
+for i in range(3):
     url_send = requests.post(
         url="https://fetch-url-ff.herokuapp.com/add",
-        params=data
+        params=data,
+        auth = HTTPBasicAuth(os.environ.get("AUTH_USERNAME"), os.environ.get("AUTH_PASSWORD"))
     )
-    time.sleep(2)
+    time.sleep(1)
