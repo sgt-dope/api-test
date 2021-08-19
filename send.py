ec1146c27f177fa3217605b79cfd331fa4db7aeb
sgt-dope <sgtdopeaf@gmail.com>
2021-08-19 10:56:27 +0000

    removed requirements.txt and .yaml file. requrests is included in python 3.9

--- send.py
+++ send.py
@@ -7,7 +7,10 @@ comnd = os.popen('gp url')
 curr_url = comnd.read().strip()
 
 data = {"url":curr_url}
-
+'''
+For some reason, when deploying the REST API to heroku, it requires multiple POST requests for the url to be save
+otherwise, it reverts to old links, or even empty list.
+'''
 for i in range(3):
     url_send = requests.post(
         url="https://fetch-url-ff.herokuapp.com/add",
