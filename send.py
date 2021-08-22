74720615cbf3c0dbb20f9787b32fb95f97527652
sgt-dope <sgtdopeaf@gmail.com>
2021-08-22 16:06:24 +0000

    Allow for many values to be sent with POST req

--- send.py
+++ send.py
@@ -3,18 +3,28 @@ import time
 import requests
 from requests.auth import HTTPBasicAuth
 
-comnd = os.popen('gp url')
-curr_url = comnd.read().strip()
+comnd = os.popen('gp url 8080')
+curr_url_kc = comnd.read().strip()
 
-data = {"url":curr_url}
+comnd2 = os.popen('gp url 5000')
+curr_url_app = comnd2.read().strip()
+
+
+payload = {"urls":{
+    "keycloak_url":curr_url_kc,
+    "app_url":curr_url_app
+}}
+
+
 '''
 For some reason, when deploying the REST API to heroku, it requires multiple POST requests for the url to be save
 otherwise, it reverts to old links, or even empty list.
 '''
-for i in range(3):
+
+for i in range(10):
     url_send = requests.post(
-        url="https://fetch-url-ff.herokuapp.com/add",
-        params=data,
+        url="https://fetch-urls-ff.herokuapp.com/add",
+        json=payload,
         auth = HTTPBasicAuth(os.environ.get("AUTH_USERNAME"), os.environ.get("AUTH_PASSWORD"))
     )
-    time.sleep(1)
+    time.sleep(0.2)
